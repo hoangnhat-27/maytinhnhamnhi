6ef7cde3d966287b8b9ca61ba4835bc38806cd69
Hoang Nhat <hoangnhat2772001@gmail.com>
2022-09-22 00:00:06 +0700

    test2

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const btnCalc = document.querySelector('#btn').addEventListener('click', () => {
     const inputAge = document.querySelector('#ageInput').value;
     const show = document.querySelector('.result');
-    let result = `Tuổi của bạn là: ${inputAge} tuổi`;
+    let result = `Tuổi của bạn DTH là: ${inputAge} tuổi`;
     show.classList.add('show');
     show.textContent = result;
 });
